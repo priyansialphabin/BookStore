da22343ba78df914a6b84e2812dd921d3c549841
Hiral Ranpariya <hiralr@valuefy.com>
2022-12-12 15:07:19 +0530

    Initial commit

---
+++ build/precache-manifest.45b1b6556760c9ff93a6a08e4b9385d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddfe9e081cfc992bb0c3",
+    "url": "/static/css/main.112bbb19.chunk.css"
+  },
+  {
+    "revision": "ddfe9e081cfc992bb0c3",
+    "url": "/static/js/main.ddfe9e08.chunk.js"
+  },
+  {
+    "revision": "d62352ef5216b81317c0",
+    "url": "/static/js/1.d62352ef.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e2f7d7cf7b08b133c6649399016be826",
+    "url": "/static/media/Library_pic1.e2f7d7cf.jpg"
+  },
+  {
+    "revision": "7bef1827171bc61f0f2941c10618724e",
+    "url": "/static/media/Library_Illustration_1.7bef1827.jpg"
+  },
+  {
+    "revision": "6134f316d0926176cc799e0694c6afed",
+    "url": "/index.html"
+  }
+];
---
+++ src/Context/user-login-context.js
@@ -0,0 +1,18 @@
+import React, { Component }  from 'react';
+import { useState, useContext, createContext } from "react"
+
+const UserLoginContext = createContext()
+
+let UserLoginContextProvider = ({children}) => {
+    const [userLoggedIn, setUserLoggedIn] = useState(false)
+    
+    return (
+        <UserLoginContext.Provider value={{userLoggedIn, setUserLoggedIn}}>
+            {children}
+        </UserLoginContext.Provider>
+    )
+}
+
+const useUserLogin = () => useContext(UserLoginContext)
+
+export { UserLoginContextProvider, useUserLogin }
